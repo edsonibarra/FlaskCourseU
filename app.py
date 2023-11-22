03c46bef5930250caa7cb68e6f27339752bbd679
edsonibarra <edson.ibarra@globant.com>
2023-11-22 17:28:18 -0600

    remove endpoint

--- app.py
+++ app.py
@@ -36,23 +36,4 @@ def create_store():
     stores.append(new_store)
     return new_store, 201
 
-@app.post("/item")
-def create_item_in_store():
-    request_data = request.get_json()
-    # validate name and items exists in the payload
-    new_store_name = request_data.get("name", None)
-    new_items = request_data.get("items", None)
-    
-    if new_store_name is None:
-        return {"message": "Invalid parameters [name]"}, 400
-    if new_items is None:
-        return {"message": "Invalid parameters [items]"}, 400
-    
-    # Look up store name
-    for store in stores:
-        if store["name"] == new_store_name:
-            store_items = store["items"]
-            for item in new_items:
-                store_items.append(item)
-            return {"message": "Item(s) created"}, 201
-    return {"message": f"Store {new_store_name} was not found"}, 404      
+  
